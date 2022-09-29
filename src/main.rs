6d2343bdd37cf3df279a078138409e76a014a7a3
Joe Gesualdo <joegesualdo@gmail.com>
2022-09-29 09:18:29 -0400

    Adjustments (this is going to be replaced soon)

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use ::sha256;
 use bitcoin::hashes::hex::FromHex;
 use bitcoin::hashes::{ripemd160, Hash};
 use bitcoin::util::base58::check_encode_slice;
+use create_bitcoin_private_key::create_private_key;
 use rand::Rng;
 use secp256k1::{Secp256k1, SecretKey};
 
@@ -14,6 +15,7 @@ use secp256k1::{Secp256k1, SecretKey};
 // - https://en.bitcoin.it/wiki/Elliptic_Curve_Digital_Signature_Algorithm
 // - https://gobittest.appspot.com/Address
 // - https://en.bitcoin.it/wiki/Protocol_documentation#Signatures
+// - https://bitcoinbriefly.com/practical-guide-bitcoin-addresses-explained/
 //
 
 pub fn concat_u8(first: &[u8], second: &[u8]) -> Vec<u8> {
@@ -34,43 +36,6 @@ pub fn encode_hex(bytes: &[u8]) -> String {
     for &b in bytes {
         write!(&mut s, "{:02x}", b).unwrap();
     }
-    s
-}
-
-fn create_private_key() -> String {
-    // we create an array that will store the byte representation of each of our numbers. We're
-    //   going to have 64 numbers, so the length will be 64, where each item represents a 16 byte
-    //   number (0 -15).
-    let mut byte_array: Vec<u8> = Vec::new();
-    // we create an string that will represent our byte_array but as a hexidecimal number
-    // let mut s = String::new();
-    // loop through 64 times and create a new hex number each time because
-    //    we want a number with 64 digits, and each of them will be 4 bytes,
-    //    for a number with 256 bytes total.
-    for _x in 0..64 {
-        // get number between 0 - 255 billion;
-        let mut rng = rand::thread_rng();
-        let random_int_1 = rng.gen_range(0..255);
-        // get number between 1 - 16;
-        let random_int_2 = rng.gen_range(1..=16);
-
-        // we dont want a number any larger than 15 (because we want a 4 byte number)
-        //    so we have to modulo our first random number by our second. Notice the second
-        //    random number can only go as high as 16 and by moduloing our random number by that
-        //    we'll never get a number larger than 16
-        let random_num = random_int_1 % random_int_2;
-        // push our random number onto the byte array
-        byte_array.push(random_num);
-        // push our random number onto the string
-        // let hex = format!("{:x}", b);
-        //s.push_str(&hex);
-    }
-
-    // convert byte array into a hex string
-    let s = byte_array
-        .iter()
-        .map(|byte| format!("{:x}", byte))
-        .collect::<String>();
     s
 }
 
